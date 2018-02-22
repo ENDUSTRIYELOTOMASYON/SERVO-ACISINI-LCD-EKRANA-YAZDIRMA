f1dca0d4d50f1eebb4b794aae3c26dbc66a0b791
AKIN KAHRAMAN <36744498+ENDUSTRIYELOTOMASYON@users.noreply.github.com>
2018-02-22 22:17:02 +0200

    Add files via upload

---
+++ LCD_SERVO_DE_ER_.ino
@@ -0,0 +1,24 @@
+#include <LiquidCrystal.h>
+#include <Servo.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+Servo servo_kontrol;//SErvo_kontrol adında servo nesnesi oluşturuldu
+int pot_pin=0;
+int servo_pin=6;
+int pot_deger=0;
+int servo_aci=0;
+
+void setup(){
+  lcd.begin(16,2);
+  servo_kontrol.attach(servo_pin);
+}
+void loop() {
+ pot_deger=analogRead(pot_pin);
+ servo_aci=map(pot_deger,0,1023,0,180);
+ servo_kontrol.write(servo_aci);
+ lcd.setCursor(0,0);
+ lcd.print("ACI DEGERI: ");
+ lcd.setCursor(13,0);
+ lcd.print(servo_aci);
+ delay(100); 
+ lcd.clear();
+}
